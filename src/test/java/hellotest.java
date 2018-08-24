e025e8d73de2d65f627082ea387ca2d19c993976
1995wade <1508541441@qq.com>
2018-08-24 10:31:51 +0800

    first commit

---
+++ src/test/java/hellotest.java
@@ -0,0 +1,9 @@
+import sun.jvm.hotspot.HelloWorld;
+
+public class hellotest {
+    public void testSayHello(){
+        HelloWorld helloWorld=new HelloWorld();
+
+        System.out.println("Hello World!");
+    }
+}
