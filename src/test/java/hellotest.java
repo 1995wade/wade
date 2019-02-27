c6909cf77862627d95bd10e0255f274f359666d9
1995wade <42404537+1995wade@users.noreply.github.com>
2019-02-27 10:23:50 +0800

    Delete hellotest.java

--- src/test/java/hellotest.java
+++
@@ -1,9 +0,0 @@
-import sun.jvm.hotspot.HelloWorld;
-
-public class hellotest {
-    public void testSayHello(){
-        HelloWorld helloWorld=new HelloWorld();
-
-        System.out.println("Hello World!");
-    }
-}
